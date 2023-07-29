fef9ebd6ed8cd37180bc35749b370ab908fb8820
o-RoGerSLegacy-o <shubhamhugay@gmail.com>
2023-07-29 14:57:00 +0530

    creating the main  page

---
+++ client/src/App.js
@@ -0,0 +1,23 @@
+import "./App.css";
+
+function App() {
+  return (
+    <main>
+      <header>
+        <a href="className">MyBlog</a>
+        <nav>
+          <a href=""> login</a>
+          <a href=""> Register</a>
+        </nav>
+      </header>
+      <div className="entry">
+        <img
+          src="https://techcrunch.com/wp-content/uploads/2021/06/GettyImages-1160976646.jpg?w=730&crop=1"
+          alt=""
+        />
+      </div>
+    </main>
+  );
+}
+
+export default App;
